0d8bd880da99490b964df5f7af62493b3bcc417e
Baptiste Hardrick <batach31@hotmail.com>
2020-03-03 13:36:42 +0100

    first commit

---
+++ lab1/src/main/scala/Utils/ClinksCalculator.scala
@@ -0,0 +1,32 @@
+package Utils
+
+/**
+  * Contains the functions necessary to calculate the number of *clinks* when n people want to cheers.
+  */
+object ClinksCalculator {
+  /**
+    * Calculate the factorial of a given number.
+    * @param n the number to compute
+    * @return n!
+    */
+  // TODO - Step 1
+  def factorial(n: Int): Int = {
+    //make it tail recursive
+    def loop(x: Int, acc: Int): Int = x match {
+      case 0 => acc
+      case _ => loop(x-1, x*acc)
+    }
+    loop(n, 1)
+  }
+
+  /**
+    * Calculate the combination of two given numbers.
+    * @param n the first number
+    * @param k the second number
+    * @return n choose k
+    */
+  // TODO - Step 1
+  def calculateCombination(n: Int, k: Int): Int = {
+    factorial(n)/(factorial(k)*factorial(n-k))
+  }
+}
